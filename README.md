a4a6e75d571d4e94d0ac1f11d1fa56d824b90b67
mrguestlucky <guestm0de0nfire@gmail.com>
2026-02-28 23:59:08 +0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# 𝙄𝙈𝙈𝙐 𝙈𝘿 - 𝙈𝙄𝙉𝙄
+✧〖THE  BEST WHATSAPP BOT CREATED BY IMAD ALI  ♥️〗✧
---
+++ config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    AUTO_VIEW_STATUS: 'true',
+    AUTO_LIKE_STATUS: 'true',
+    AUTO_RECORDING: 'true',
+    AUTO_LIKE_EMOJI: ['🧩', '🍉', '💜', '🌸', '🪴', '💊', '💫', '🍂', '🌟', '🎋', '😶‍🌫️', '🫀', '🧿', '👀', '😘', '🚩', '🥰', '🗿', '💜', '💙', '🌝', '🖤', '💗'],
+    PREFIX: '.',
+    MAX_RETRIES: 3,
+    GROUP_INVITE_LINK: 'https://chat.whatsapp.com/JQTH0GwURpjIJEzhpcFosO?mode=hq2tcla',
+    ADMIN_LIST_PATH: './admin.json',
+    IMAGE_PATH: 'https://i.postimg.cc/Y0r8FCB5/Picsart-26-02-28-23-12-59-324.jpg',
+    NEWSLETTER_JID: '120363341506278064@newsletter',
+    NEWSLETTER_MESSAGE_ID: '5775',
+    OTP_EXPIRY: 300000,
+    NEWS_JSON_URL: '',
+    BOT_NAME: 'ɪᴍᴍᴜ ᴍᴅ ᴍɪɴɪ',
+    OWNER_NAME: 'IᴍAᴅ ᴀLɪ',
+    OWNER_NUMBER: '923493114170',
+    BOT_VERSION: '2.0.0 ʙᴇᴛᴀ',
+    BOT_FOOTER: '> © ᴘᴏᴡᴇʀᴇᴅ ʙʏ ɪᴍᴍᴜ',
+    CHANNEL_LINK: 'https://whatsapp.com/channel/0029Vaq4PRsD38CJKXzwmb42'
+};
